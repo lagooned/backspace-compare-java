f4fcef64db8fa9717b04211cd1db9c8c08c1fbac
Jared Engler <jared.lite@gmail.com>
2020-08-21 15:42:46 -0500

    finished the easy way

--- src/main/java/com/jaredengler/StringWithBackspacesComparitor.java
+++ src/main/java/com/jaredengler/StringWithBackspacesComparitor.java
@@ -3,4 +3,8 @@ package com.jaredengler;
 
 public class StringWithBackspacesComparitor {
 
+    public Boolean compare(String string, String string2) {
+        return string.replaceAll(".\b", "").equals(string2.replaceAll(".\b", ""));
+    }
+
 }
